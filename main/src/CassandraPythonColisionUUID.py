4871f658995bfc52727c703ffd1ac5c86d10b2b2
DESKTOP-8LE97JM\tiagoooliveira <tiagooliveira2@natura.net>
2017-05-31 22:08:24 -0300

    Fix the range 0 based index.

--- main/src/CassandraPythonColisionUUID.py
+++ main/src/CassandraPythonColisionUUID.py
@@ -3,11 +3,11 @@ from concurrent import futures
 from cassandra.cluster import Cluster
 from random import randint
 
-MAX_WORKERS = 2
+MAX_WORKERS = 30
 CASSANDRA_HOST = '127.0.0.1'
 CASSANDRA_PORT = 32770
 CASSANDRA_KEYSPACE = 'colisiontest'
-CYCLE_PER_THREAD = 5000
+CYCLE_PER_THREAD = 1000
 
 cluster = Cluster(
     [CASSANDRA_HOST],
@@ -19,13 +19,13 @@ cluster = Cluster(
 session = cluster.connect(CASSANDRA_KEYSPACE)
 
 def uuid_insert(wks):
-    rand = randint(0,9)
+    rand = randint(0,20)
     time.sleep(rand)
     t0 = time.time()
-    for num in range(1, CYCLE_PER_THREAD):
+    for num in range(0, CYCLE_PER_THREAD):
         #time.sleep(randint(0,20))
         #todo: call select uuid() from colisiontest.testeuuid limit 1; Then insert ...  IF NOT EXISTS
-        session.execute("INSERT INTO colisiontest.testeuuid(universal_id, moment) VALUES(uuid(), unixTimestampOf(now()));")
+        session.execute("INSERT INTO {}.testeuuid(universal_id, moment) VALUES(uuid(), unixTimestampOf(now()));".format(CASSANDRA_KEYSPACE))
     elapsed = time.time() - t0
     msg = '\n Thread {} started sleep {} ;call {} inserts in {:.2f}s'.format(wks, rand, CYCLE_PER_THREAD, elapsed)
     print(msg)
@@ -64,19 +64,40 @@ if __name__ == '__main__':
 
 ########################################################################################################
 #Env execution: Win10 64bits; 16GB mem; i7
-#Cassandra Docker
+#Cassandra Docker https://hub.docker.com/r/spotify/cassandra/
 
-#without sleeprandon; then 10 colisions (why?)
-#Thread 6 call 5000 inserts in 33.99s
-#Thread 5 call 5000 inserts in 34.03s
-#Thread 7 call 5000 inserts in 34.10s
-#Thread 9 call 5000 inserts in 34.14s
-#Thread 3 call 5000 inserts in 34.21s
-#Thread 2 call 5000 inserts in 34.21s
-#Thread 1 call 5000 inserts in 34.23s
-#Thread 4 call 5000 inserts in 34.24s
-#Thread 8 call 5000 inserts in 34.24s
-#Thread 0 call 5000 inserts in 34.24s
-#Total Time 34.25s
+#No colision
+    #Thread 22 started sleep 0 ;call 1000 inserts in 4.02s
+    #Thread 20 started sleep 1 ;call 1000 inserts in 5.33s
+    #Thread 15 started sleep 2 ;call 1000 inserts in 5.94s
+    #Thread 2 started sleep 3 ;call 1000 inserts in 6.63s
+    #Thread 21 started sleep 3 ;call 1000 inserts in 6.64s
+    #Thread 1 started sleep 3 ;call 1000 inserts in 6.68s
+    #Thread 17 started sleep 4 ;call 1000 inserts in 6.79s
+    #Thread 4 started sleep 4 ;call 1000 inserts in 6.86s
+    #Thread 14 started sleep 4 ;call 1000 inserts in 6.95s
+    #Thread 19 started sleep 5 ;call 1000 inserts in 6.37s
+    #Thread 27 started sleep 5 ;call 1000 inserts in 6.43s
+    #Thread 5 started sleep 7 ;call 1000 inserts in 6.07s
+    #Thread 12 started sleep 8 ;call 1000 inserts in 5.81s
+    #Thread 7 started sleep 8 ;call 1000 inserts in 5.85s
+    #Thread 9 started sleep 8 ;call 1000 inserts in 5.87s
+    #Thread 18 started sleep 9 ;call 1000 inserts in 5.27s
+    #Thread 8 started sleep 10 ;call 1000 inserts in 4.81s
+    #Thread 25 started sleep 11 ;call 1000 inserts in 4.33s
+    #Thread 24 started sleep 12 ;call 1000 inserts in 3.88s
+    #Thread 13 started sleep 13 ;call 1000 inserts in 3.53s
+    #Thread 0 started sleep 15 ;call 1000 inserts in 3.48s
+    #Thread 16 started sleep 16 ;call 1000 inserts in 4.34s
+    #Thread 28 started sleep 17 ;call 1000 inserts in 4.72s
+    #Thread 3 started sleep 17 ;call 1000 inserts in 4.76s
+    #Thread 29 started sleep 17 ;call 1000 inserts in 4.79s
+    #Thread 10 started sleep 17 ;call 1000 inserts in 4.90s
+    #Thread 6 started sleep 18 ;call 1000 inserts in 4.75s
+    #Thread 23 started sleep 18 ;call 1000 inserts in 4.84s
+    #Thread 11 started sleep 19 ;call 1000 inserts in 4.38s
+    #Thread 26 started sleep 20 ;call 1000 inserts in 4.01s
+
+#Total Time 24.03s
     #Cassandra select count(*) from colisiontest.testeuuid;
-    #49990 rows:
+    #30000 rows:
